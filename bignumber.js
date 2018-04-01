cc23c622e4205912b34d626be94a36beea21a3dc
terryyannan <terry.yannan@gmail.com>
2018-04-01 13:25:35 +0800

    remove redundant checks

--- bignumber.js
+++ bignumber.js
@@ -1271,11 +1271,11 @@
 
         // Handle values that fail the validity test in BigNumber.
         parseNumeric = (function () {
-            var basePrefix = /^(-?)0([xbo])(?=\w[\w.]*$)/i,
+            var basePrefix = /^(-?)0([xbo])/i,
                 dotAfter = /^([^.]+)\.$/,
                 dotBefore = /^\.([^.]+)$/,
                 isInfinityOrNaN = /^-?(Infinity|NaN)$/,
-                whitespaceOrPlus = /^\s*\+(?=[\w.])|^\s+|\s+$/g;
+                whitespaceOrPlus = /^\s*\+|^\s+|\s+$/g;
 
             return function ( x, str, isNum, b ) {
                 var base,
